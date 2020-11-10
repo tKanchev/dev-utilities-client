4d32cc1d805a3fe7a947b786dd3c3a147aae9b7a
tKanchev <t.kanchev@yahoo.com>
2020-11-10 16:45:08 +0200

    Add Document title

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -3,6 +3,10 @@ import GuidContainer from '../guid-container/guid-container';
 import Welcome from '../welcome/welcome';
 
 class Home extends Component {
+    componentDidMount(){
+        document.title = "Free Online Guid Generator"
+    }
+    
     render (): JSX.Element {
         return (
             <div className='home'>
